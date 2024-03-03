9e063adb47fdb1c89c70065ca6045d6cc22f71ac
amansekhon888 <149692128+amansekhon888@users.noreply.github.com>
2024-03-03 12:52:03 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,40 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PUBGToken is ERC20, Ownable {
+
+    uint256 killPoints;
+    uint256 coins;
+    uint256 bullets;
+
+    constructor() ERC20("PUBGToken", "PUBG") Ownable() {
+        _mint(msg.sender, 10);
+        coins = 10;
+        killPoints = 0;
+        bullets = 1;
+    }
+
+    function redeemKillPoint() public {
+        require(killPoints > 0, "No kill points to redeem");
+        coins += killPoints;
+        _mint(msg.sender, killPoints);
+        killPoints = 0;
+    }
+
+    function buyAmmo(uint256 amt) public {
+        require(coins >= amt, "Amount should be less than coins");
+        coins -= amt;
+        _burn(msg.sender, amt);
+        bullets += amt;
+    }
+
+    function shootEnemy() public {
+        require(bullets > 0, "At least one bullet required to kill enemy");
+        bullets--;
+        killPoints += 2;
+    }
+}
